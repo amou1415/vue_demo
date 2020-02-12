fcf0f04bd67e896af37078b642b7d6a6da0e7860
amou1415 <775739656@qq.com>
2020-02-12 09:33:50 +0800

    项目开始

---
+++ news42/src/components/authInput.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+      <input type="text">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+input {
+    width: 100%;
+    height: 10.556vw;
+    padding: 20px;
+    box-sizing: border-box;
+    background-color: #fff;
+    border: none;
+    outline: none;
+    border-bottom:1px solid black; 
+}
+</style>
---
+++ news42/src/views/login.vue
@@ -0,0 +1,20 @@
+<template>
+  
+</template>
+
+<script>
+import authInput from '@components/authInput.vue'
+export default {
+    componnents:{
+        authInput
+    }
+};
+</script>
+
+<style lang="less">
+
+.login {
+    margin: 6.667vw;
+}
+
+</style>
