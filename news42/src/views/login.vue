f4d093dfe278872b74b38c9956a3d6d92106ce78
amou1415 <775739656@qq.com>
2020-02-12 14:51:33 +0800

    登陆页添加logo和关闭按钮

--- news42/src/views/login.vue
+++ news42/src/views/login.vue
@@ -1,22 +1,38 @@
 <template>
   <div>
-      
+    <div class="closeBtn">
+      <span class="iconfont iconicon-test"></span>
+    </div>
+    <div class="logo">
+      <span class="iconfont iconnew"></span>
+    </div>
   </div>
 </template>
 
 <script>
-import authInput from '@components/authInput.vue'
+import authInput from "@components/authInput.vue";
 export default {
-    componnents:{
-        authInput
-    }
+  componnents: {
+    authInput
+  }
 };
 </script>
 
 <style lang="less">
-
 .login {
-    margin: 6.667vw;
+  margin: 6.667vw;
 }
-
+.closeBtn {
+  padding: 6.667vw;
+  span {
+    font-size: 7.5vw;
+  }
+}
+.logo {
+  text-align: center;
+  sapn {
+    font-size: 35vw;
+    color: #d81e06;
+  }
+}
 </style>
